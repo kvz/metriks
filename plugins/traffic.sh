4727ff378d1479270d3c7412fea389a967ecf919
Kevin van Zonneveld <kevin@vanzonneveld.net>
2014-02-18 23:58:46 +0100

    Group by interface, then in/out

--- plugins/traffic.sh
+++ plugins/traffic.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
-# @todo: We need to support COUNTER for this one
 echo "# graph->title: Network traffic in/out per interface"
 echo "# graph->verticalLabel: Bytes"
 
@@ -8,9 +7,13 @@ echo "# lineStore->*->consolidation: AVERAGE"
 echo "# lineStore->*->dsType: COUNTER"
 
 if [[ "$OSTYPE" == "darwin"* ]]; then
-  netstat -ib |awk '{print $1"-in " $7}' |egrep -v '^(gif|stf|p2p|lo|vbox|Name)' |sort -u
-  netstat -ib |awk '{print $1"-out " $10}' |egrep -v '^(gif|stf|p2p|lo|vbox|Name)' |sort -u
+  (
+    netstat -ib |awk '{print $1"-in " $7}' &&
+    netstat -ib |awk '{print $1"-out " $10}'
+  ) |egrep -v '^(gif|stf|bridg|ham|p2p|lo|vbox|Name)' |sort -u
 else
-  cat /proc/net/dev |tail -n-2 |sed 's/://g' |awk '{print $1"-in " $2}' |egrep -v '^(lo)'
-  cat /proc/net/dev |tail -n-2  |sed 's/://g' |awk '{print $1"-out " $10}' |egrep -v '^(lo)'
+  (
+    cat /proc/net/dev |awk '{print $1"-in " $2}' &&
+    cat /proc/net/dev |awk '{print $1"-out " $10}'
+  ) |tail -n-2 |sed 's/://g' |egrep -v '^(lo)'
 fi
