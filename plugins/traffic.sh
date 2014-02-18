4d72e76333f3774183d1c0ae8d01243c48751974
Kevin van Zonneveld <kevin@vanzonneveld.net>
2014-02-18 23:53:14 +0100

    Ignore p2p

--- plugins/traffic.sh
+++ plugins/traffic.sh
@@ -8,8 +8,8 @@ echo "# lineStore->*->consolidation: AVERAGE"
 echo "# lineStore->*->dsType: COUNTER"
 
 if [[ "$OSTYPE" == "darwin"* ]]; then
-  netstat -ib |awk '{print $1"-in " $7}' |egrep -v '^(gif|stf|lo|vbox|Name)' |sort -u
-  netstat -ib |awk '{print $1"-out " $10}' |egrep -v '^(gif|stf|lo|vbox|Name)' |sort -u
+  netstat -ib |awk '{print $1"-in " $7}' |egrep -v '^(gif|stf|p2p|lo|vbox|Name)' |sort -u
+  netstat -ib |awk '{print $1"-out " $10}' |egrep -v '^(gif|stf|p2p|lo|vbox|Name)' |sort -u
 else
   cat /proc/net/dev |tail -n-2 |sed 's/://g' |awk '{print $1"-in " $2}' |egrep -v '^(lo)'
   cat /proc/net/dev |tail -n-2  |sed 's/://g' |awk '{print $1"-out " $10}' |egrep -v '^(lo)'
