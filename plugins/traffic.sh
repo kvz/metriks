ec3ee22682fa705a09d57551d868443ef39d9357
Kevin van Zonneveld <kevin@vanzonneveld.net>
2014-02-19 02:53:09 +0100

    Make traffic work in bits

--- plugins/traffic.sh
+++ plugins/traffic.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 echo "# graph->title: Network traffic in/out per interface"
-echo "# graph->verticalLabel: Bytes"
+echo "# graph->verticalLabel: bits / s"
 
 echo "# line->*->element: AREA"
 echo "# lineStore->*->consolidation: AVERAGE"
@@ -8,12 +8,12 @@ echo "# lineStore->*->dsType: COUNTER"
 
 if [[ "$OSTYPE" == "darwin"* ]]; then
   (
-    netstat -ib |awk '{print $1"-in " $7}' &&
-    netstat -ib |awk '{print $1"-out " $10}'
+    netstat -ib |awk '{print $1"-in " $7*8}' &&
+    netstat -ib |awk '{print $1"-out " $10*8}'
   ) |egrep -v '^(gif|stf|bridg|ham|p2p|lo|vbox|Name)' |sort -u
 else
   (
-    cat /proc/net/dev |awk '{print $1"-in " $2}' &&
-    cat /proc/net/dev |awk '{print $1"-out " $10}'
+    cat /proc/net/dev |awk '{print $1"-in " $2*8}' &&
+    cat /proc/net/dev |awk '{print $1"-out " $10*8}'
   ) |tail -n-2 |sed 's/://g' |egrep -v '^(lo)'
 fi
